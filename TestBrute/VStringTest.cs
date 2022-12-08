60ebb67ac3a374c68c10cd4211ca696193fda79f
Nick Igelbrink <zi.aal2000@gmail.com>
2022-12-08 16:58:46 +0100

    vstring generation

---
+++ TestBrute/VStringTest.cs
@@ -0,0 +1,61 @@
+﻿using Jump_Bruteforcer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestBrute
+{
+    public class VStringTest
+    {
+        [Fact]
+        public void VStringTest1()
+        {
+            List<VPlayer> VStrings = VPlayer.GenerateVStrings(407.4, true, 327.5);
+            Assert.Equal(15, VStrings.Count);
+        }
+
+        [Fact]
+        public void VStringTest2()
+        {
+            List<VPlayer> VStrings = VPlayer.GenerateVStrings(407.4, true, 321.3);
+            Assert.Equal(2, VStrings.Count);
+        }
+
+        [Fact]
+        public void VStringTest3()
+        {
+            List<VPlayer> VStrings = VPlayer.GenerateVStrings(407.4, true, 320);
+            Assert.Empty(VStrings);
+        }
+
+        [Fact]
+        public void VStringTest4()
+        {
+            List<VPlayer> VStrings = VPlayer.GenerateVStrings(407.4, false, 340);
+            Assert.Empty(VStrings);
+        }
+
+        [Fact]
+        public void VStringTest5()
+        {
+            List<VPlayer> VStrings = VPlayer.GenerateVStrings(407.4, false, 350);
+            Assert.Equal(3, VStrings.Count);
+        }
+
+        [Fact]
+        public void VStringTest6()
+        {
+            List<VPlayer> VStrings = VPlayer.GenerateVStrings(407.4, false, 400);
+            Assert.Equal(120, VStrings.Count);
+        }
+
+        [Fact]
+        public void VStringTest7()
+        {
+            List<VPlayer> VStrings = VPlayer.GenerateVStrings(407.4, false, 550);
+            Assert.Equal(127, VStrings.Count);
+        }
+    }
+}
---
+++ VString.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jump_Bruteforcer
+{
+    public class VPlayer
+    {
+        const double GRAVITY = 0.4, MAX_VSPEED = 9, RELEASE_MULTIPLIER = 0.45, SJUMP_VSPEED = -8.5, DJUMP_VSPEED = -7;
+        double Y, VSpeed;
+        bool GoalHeightReached;
+
+        readonly List<double> VString;
+        readonly List<bool> Releases;
+
+        // could also use int and use rounded position to check if on same pixel
+        static double LowestGoal;
+
+        public VPlayer(double Y, double VSpeed, double LowestGoal)
+        {
+            this.Y = Y;
+            this.VSpeed = VSpeed;
+            GoalHeightReached = Y <= LowestGoal;
+
+            VString = new List<double>();
+            Releases = new List<bool>();
+
+            VPlayer.LowestGoal = LowestGoal;
+        }
+
+        public VPlayer(VPlayer vp)
+        {
+            Y = vp.Y;
+            VSpeed = vp.VSpeed;
+            GoalHeightReached = vp.GoalHeightReached;
+
+            VString = new List<double>(vp.VString);
+            Releases = new List<bool>(vp.Releases);
+        }
+
+        public void Jump(bool SingleJump)
+        {
+            VSpeed = SingleJump ? SJUMP_VSPEED : DJUMP_VSPEED;
+        }
+
+        public bool CanRelease()
+            => VSpeed < 0;
+
+        public bool Advance(bool Release)
+        {
+            // assuming releases only used when CanRelease true
+            if (Release)
+            {
+                VSpeed *= RELEASE_MULTIPLIER;
+            }
+            else if (VSpeed > MAX_VSPEED)
+            {
+                VSpeed = MAX_VSPEED;
+            }
+
+            VSpeed += GRAVITY;
+
+            Y += VSpeed;
+
+            bool AboveGoal = Y <= LowestGoal;
+            if (GoalHeightReached && !AboveGoal)
+            {
+                // first frame under the goal height,
+                // returning early since this frame is not useful in the vstring
+                return false;
+            }
+
+            VString.Add(Y);
+            Releases.Add(Release);
+
+            GoalHeightReached = GoalHeightReached || AboveGoal;
+            return AboveGoal;
+        }
+
+        public static List<VPlayer> GenerateVStrings(double Y, bool SingleJump, double LowestGoal)
+        {
+            VPlayer Root = new(Y, 0, LowestGoal);
+            Root.Jump(SingleJump);
+
+            List<VPlayer> Result = new();
+            Queue<VPlayer> Players = new();
+            Players.Enqueue(Root);
+
+            while (Players.Count > 0)
+            {
+                VPlayer Player = Players.Peek();
+
+                if (Player.CanRelease())
+                {
+                    do
+                    {
+                        VPlayer NewPlayer = new VPlayer(Player);
+                        NewPlayer.Advance(false);
+                        Players.Enqueue(NewPlayer);
+
+                        Player.Advance(true);
+                    }
+                    while (Player.CanRelease());
+                }
+                else
+                {
+                    Players.Dequeue();
+                    if (Player.GoalHeightReached)
+                    {
+                        while (Player.Advance(false)) ;
+                        Result.Add(Player);
+                    }
+                }
+            }
+
+            return Result;
+        }
+    }
+}

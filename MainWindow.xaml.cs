8177649985d1cbe403d026ce9ed6dfa38d32a4fc
Daggerfall-Is-The-Best-TES-Game <Davidcfried@gmail.com>
2023-01-12 07:12:04 -0500

    refactor project to WPF from Windows Forms

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Jump_Bruteforcer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonSelectJMap_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog o = new()
+            {
+                DefaultExt = ".jmap"
+            };
+
+            Nullable<bool> result = o.ShowDialog();
+
+            if (result == true)
+            {
+                string FileName = o.FileName;
+                LabelFileName.Content = FileName;
+
+                string Text = File.ReadAllText(FileName);
+
+                Map Map = JMap.Parse(Text);
+                BitmapSource s = Imaging.CreateBitmapSourceFromHBitmap(Map.Bmp.GetHbitmap(), IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions()); //https://stackoverflow.com/questions/6484357/converting-bitmapimage-to-bitmap-and-vice-versa
+                ImageJMap.Source = s;
+                Clipboard.SetImage(s);
+            }
+
+
+        }
+    }
+}
--- Map.cs
+++ Map.cs
@@ -1,7 +1,9 @@
 ﻿
 using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.Drawing;
 using System.Drawing.Imaging;
+using System.IO;
 using System.Runtime.InteropServices;
 using System.Text;
 

2dede40ee76370bd6da18e35dd3bcf3eae91802f
Daggerfall-Is-The-Best-TES-Game <Davidcfried@gmail.com>
2023-01-22 16:23:53 -0500

    search node state equality implemented

---
+++ PlayerNode.cs
@@ -0,0 +1,58 @@
+﻿using Priority_Queue;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace Jump_Bruteforcer
+{
+
+    public class PlayerNode : IEquatable<PlayerNode>
+    {
+        public int X { get; init; }
+        public double Y { get; init; } 
+        public double VSpeed { get; init; } 
+        public PlayerNode? Parent { get; set; }
+        public double PathCost {get; set; }
+        public Input? Action { get; set; }
+
+        public PlayerNode(int x, double y, double vSpeed) {
+            X = x;
+            Y = y;
+            VSpeed = vSpeed;
+            Parent = null;
+            PathCost= 0;
+            Action= null;
+        }
+        
+        public (List<Input>, PointCollection) GetPath()
+        {
+            throw new NotImplementedException();
+        } 
+
+        public HashSet<PlayerNode> GetNeighbors() 
+        { 
+            throw new NotImplementedException(); 
+        }
+
+
+
+        public bool Equals(PlayerNode? other)
+        {
+            if (other is null)
+            {
+                return false;
+            }
+
+            return this.VSpeed == other.VSpeed && this.X == other.X && this.Y == other.Y;
+        }
+
+        public override int GetHashCode()
+        {
+            return (X, Y, VSpeed).GetHashCode();
+        }
+    }
+}
---
+++ TestBrute/TestPlayerNode.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Jump_Bruteforcer;
+using Priority_Queue;
+
+namespace TestBrute
+{
+    public class TestPlayerNode
+    {
+        [Theory]
+        [InlineData(1,2,3, false)]
+        [InlineData(0,0,0, true)]
+        public void TestNodeEquals(int x, double y, double vSpeed, bool shouldEqual)
+        {
+            var n1 = new PlayerNode(0,0,0);
+            var n2 = new PlayerNode(x, y, vSpeed);
+            (n1.Equals(n2)).Should().Be(shouldEqual);
+
+        }
+
+        [Fact]
+        public void TestQueueContains()
+        {
+            var n1 = new PlayerNode(0, 0, 0);
+            var n2 = new PlayerNode(0, 0, 0);
+            var n3 = new PlayerNode(1, 0, 0);
+            var q = new SimplePriorityQueue<PlayerNode, float>();
+            q.Enqueue(n1, 1f);
+            q.Contains(n2).Should().BeTrue();
+            q.Contains(n3).Should().BeFalse();
+
+        }
+
+
+    }
+}

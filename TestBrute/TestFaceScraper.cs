60305799505573ca5ce7fb707a113df7a5159dfe
Daggerfall-Is-The-Best-TES-Game <Davidcfried@gmail.com>
2023-10-25 18:06:10 -0400

    change scraper test names

--- TestBrute/TestFaceScraper.cs
+++ TestBrute/TestFaceScraper.cs
@@ -22,7 +22,7 @@ namespace TestBrute
         }
 
         [Fact]
-        public void TestNoInputs()
+        public void TestNoInputsFaceScraperFacingLeft()
         {
             string path = @$"..\..\..\instance_maps\rHell1.txt";
             string Text = File.ReadAllText(path);
@@ -41,7 +41,7 @@ namespace TestBrute
         }
 
         [Fact]
-        public void TestNoInputsFaceScraperRight()
+        public void TestNoInputsFaceScraperFacingRight()
         {
             string path = @$"..\..\..\instance_maps\rHell1.txt";
             string Text = File.ReadAllText(path);
@@ -63,7 +63,7 @@ namespace TestBrute
 
         /* TODO: implement facing left
         [Fact]
-        public void TestNoInputsFaceScraperLeft()
+        public void TestNoInputsFaceScraperFacingLeft()
         {
             string path = @$"..\..\..\instance_maps\rHell1.txt";
             string Text = File.ReadAllText(path);
@@ -84,7 +84,7 @@ namespace TestBrute
         */
 
         [Fact]
-        public void TestFaceScraperRunIntoWallRight()
+        public void TestFaceScraperRunIntoWallFacingRight()
         {
             string path = @$"..\..\..\instance_maps\rHell1.txt";
             string Text = File.ReadAllText(path);
@@ -106,7 +106,7 @@ namespace TestBrute
 
         /* TODO: implement facing left
         [Fact]
-        public void TestFaceScraperRunIntoWallLeft()
+        public void TestFaceScraperRunIntoWallFacingLeft()
         {
             string path = @$"..\..\..\instance_maps\rHell1.txt";
             string Text = File.ReadAllText(path);
@@ -128,7 +128,7 @@ namespace TestBrute
         */
 
         [Fact]
-        public void TestFaceScraperBonkCeilingRight()
+        public void TestFaceScraperBonkCeilingFacingRight()
         {
             string path = @$"..\..\..\instance_maps\rHell1.txt";
             string Text = File.ReadAllText(path);
@@ -149,7 +149,7 @@ namespace TestBrute
 
         /* TODO: implement facing left
         [Fact]
-        public void TestFaceScraperBonkCeilingLeft()
+        public void TestFaceScraperBonkCeilingFacingLeft()
         {
             string path = @$"..\..\..\instance_maps\rHell1.txt";
             string Text = File.ReadAllText(path);
